b3c9efa5234cd981af9f092b1cefc3b793d6f447
Carlos Prieto <cprietorodriguez@gmail.com>
2024-08-24 20:29:07 +0000

    first commit

---
+++ .idx/dev.nix
@@ -0,0 +1,50 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.gnumake
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-python.python"
+      "dracula-theme.theme-dracula"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and show it in IDX's web preview panel
+          command = ["./devserver.sh"]; 
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT"; 
+         };
+       };
+      };    
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        install-oh-my-bash = ''
+          make install-oh-my-bash
+        '';
+        create-venv = ''
+          make install
+          make migrate 
+        '';
+      };
+    };
+  };
+}
---
+++ Makefile
@@ -0,0 +1,27 @@
+install:
+	if [ ! -d .venv ]; then \
+		python -m venv .venv; \
+		source $(PWD)/.venv/bin/activate; \
+		pip install -r requirements.txt; \
+	fi
+
+migrate:
+	source .venv/bin/activate && \
+	export DJANGO_SETTINGS_MODULE='mysite.settings.dev'; \
+	python manage.py migrate
+
+makemigrations:
+	source .venv/bin/activate && \
+	export DJANGO_SETTINGS_MODULE='mysite.settings.dev'; \
+	python manage.py makemigrations
+
+run:
+	source .venv/bin/activate && \
+	export DJANGO_SETTINGS_MODULE='mysite.settings.dev'; \
+	python manage.py runserver $(PORT)
+
+install-oh-my-bash:
+	if [ ! -d ~/.oh-my-bash ]; then \
+		git clone https://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash; \
+		cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc; \
+	fi
